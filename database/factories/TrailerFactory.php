307ef85a745cbfe87a69a27d2d5d672738fbbc5f
PhilipVigus <philipvigus@yahoo.co.uk>
2021-06-12 12:37:38 +0100

    Create the trailer factory

--- database/factories/TrailerFactory.php
+++ database/factories/TrailerFactory.php
@@ -2,27 +2,29 @@
 
 namespace Database\Factories;
 
+use App\Models\Film;
 use App\Models\Trailer;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TrailerFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
+    /** @var string */
     protected $model = Trailer::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
+    public function definition(): array
     {
+        $title = $this->faker->unique()->sentence;
+
         return [
-            //
+            'film_id' => Film::factory(),
+            'guid' => $this->faker->unique()->url,
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'type' => $this->faker->word,
+            'image' => $this->faker->imageUrl,
+            'link' => $this->faker->unique()->url,
+            'uploaded_at' => $this->faker->dateTimeThisDecade(),
         ];
     }
 }
