78207965d84b254780f7609eac3253bac862b04d
PhilipVigus <philipvigus@yahoo.co.uk>
2021-06-26 18:32:07 +0100

    Style priorities

--- resources/views/modals/shortlist.blade.php
+++ resources/views/modals/shortlist.blade.php
@@ -1,20 +1,22 @@
 <div @click.away="close()" class="bg-white rounded shadow-lg p-4">
     <h1 class="text-center font-bold text-lg">{{ $film['title']}}</h1>
 
-    <h2 class="text-center">Set priority</h2>
+    <div x-data="{ priority: 'low' }">
+        <h2 class="text-center">Set priority</h2>
 
-    <div class="flex justify-stretch mt-2">
-        <button class="w-1/3 bg-blue-200 hover:bg-blue-300">Low</button>
-        <button class="w-1/3 bg-yellow-300 hover:bg-yellow-400">Medium</button>
-        <button class="w-1/3 bg-red-600 hover:bg-red-700">High</button>
-    </div>
+        <div class="flex justify-stretch mt-2">
+            <button class="w-1/3 bg-blue-200 hover:bg-blue-300" x-bind:class="{ 'border-2': priority === 'low', 'border-blue-400': priority === 'low', 'font-bold': priority === 'low' }" @click="priority = 'low'">Low</button>
+            <button class="w-1/3 bg-yellow-300 hover:bg-yellow-400" x-bind:class="{ 'border-2': priority === 'medium', 'border-yellow-500': priority === 'medium', 'font-bold': priority === 'medium' }" @click="priority = 'medium'">Medium</button>
+            <button class="w-1/3 bg-red-600 hover:bg-red-700" x-bind:class="{ 'border-2': priority === 'high', 'border-red-800': priority === 'high', 'font-bold': priority === 'high' }" @click="priority = 'high'">High</button>
+        </div>
 
-    <div class="mt-4 flex justify-around">
-        <button @click="close()">
-            Cancel
-        </button>
-        <button @click="Livewire.emitTo('to-shortlist', 'shortlist', {{ $film['id'] }} ); close()">
-            Shortlist
-        </button>
+        <div class="mt-4 flex justify-around">
+            <button @click="$parent.close()">
+                Cancel
+            </button>
+            <button @click="Livewire.emitTo('to-shortlist', 'shortlist', {{ $film['id'] }} ); $parent.close()">
+                Shortlist
+            </button>
+        </div>
     </div>
 </div>
