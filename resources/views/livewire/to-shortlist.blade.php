da0f5bf35e91a570773bb6c0bd7a289f4eb9f7c2
PhilipVigus <philipvigus@yahoo.co.uk>
2021-07-10 14:39:52 +0100

    Fix DOM-diff issue in lists

--- resources/views/livewire/to-shortlist.blade.php
+++ resources/views/livewire/to-shortlist.blade.php
@@ -8,7 +8,7 @@
     <div class="py-8">
         <div class="max-w-7xl mx-auto px-4">
             @foreach ($films as $film)
-                <div class="mt-4 border" wire:key="$film->guid">
+                <div class="mt-4 border" wire:key="{{ $loop->index }}">
                     <div>{{ $film->title }}</div>
 
                     <div class="flex space-x-2">
