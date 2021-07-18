e185c0aced597f44990e4b9dc10f7a0da646a67f
PhilipVigus <philipvigus@yahoo.co.uk>
2021-07-18 12:36:52 +0100

    Add and use global scope for trailers

--- app/Models/Trailer.php
+++ app/Models/Trailer.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Scopes\TrailersWithNoIgnoredTagsScope;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
@@ -12,6 +13,11 @@ class Trailer extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new TrailersWithNoIgnoredTagsScope());
+    }
 
     public function film(): BelongsTo
     {
