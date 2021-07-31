c412817e48bd7eed003b442b126b07b3285dcf03
PhilipVigus <philipvigus@yahoo.co.uk>
2021-07-31 17:06:14 +0100

    Fix priority validation

--- resources/views/livewire/modals/priority-details.blade.php
+++ resources/views/livewire/modals/priority-details.blade.php
@@ -1,23 +1,19 @@
-<div x-data="{ rating: '{{ $priority['rating'] ?? '1' }}', comment: '{{ $priority['comment'] ?? '' }}' }">
+<div x-data="{ comment: @entangle('comment').defer, rating: @entangle('rating').defer }">
     <h1 class="text-center font-bold text-lg">{{ $film['title']}}</h1>
     
     <div>
         <h2 class="text-center">Set rating</h2>
 
         <select name="ratings" id="ratings" x-model="rating">
+            <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
         </select>
-        <div>
-        <x-icons.star />
-        <x-icons.star />
-        <x-icons.star />
-        <x-icons.star />
-        <x-icons.star />
-        </div>
+
+        @error('rating') <span class="error">{{ $message }}</span> @enderror
     </div>
 
     <div>
@@ -31,7 +27,7 @@
             Cancel
         </button>
         
-        <button x-on:click="$wire.shortlist({{ $film['id'] }}, rating, comment); $parent.close()">
+        <button x-on:click="$wire.submit()">
             Save
         </button>
     </div>
