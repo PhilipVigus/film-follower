d59a5d4be51b83f9a9d60fb6291c09a29d971a19
PhilipVigus <philipvigus@yahoo.co.uk>
2021-08-07 16:35:36 +0100

    Restyle "undo" modals

--- resources/views/livewire/modals/remove-review.blade.php
+++ resources/views/livewire/modals/remove-review.blade.php
@@ -1,17 +1,17 @@
 <div x-data="{ deleteReviewDetails: true }">
     <h1 class="text-center font-bold text-lg">{{ $film['title']}}</h1>
 
-    <div>
+    <div class="mt-2">
         <input type="checkbox" id="deleteReviewDetails" x-model="deleteReviewDetails"/>
-        <label for="deleteReviewDetails">Delete review details</label><br>
+        <label class="pl-1" for="deleteReviewDetails">Delete review details</label><br>
     </div>
 
-    <div class="mt-4 flex justify-around">
-        <button @click="$parent.close()">
+    <div class="mt-4 flex justify-between space-x-4">
+        <button class="w-full bg-gray-300 p-2 rounded-md hover:bg-gray-400" @click="$parent.close()">
             Cancel
         </button>
 
-        <button x-on:click="$wire.removeReview({{ $film['id'] }}, deleteReviewDetails); $parent.close()">
+        <button class="w-full bg-blue-800 text-gray-100 p-2 rounded-md hover:bg-blue-900" x-on:click="$wire.removeReview({{ $film['id'] }}, deleteReviewDetails); $parent.close()">
             Remove review
         </button>
     </div>
