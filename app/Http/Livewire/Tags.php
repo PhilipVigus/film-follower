6e8f580597aa29ac39283200d0bf9da675482fff
PhilipVigus <philipvigus@yahoo.co.uk>
2021-08-09 07:36:04 +0100

    Show top 50 tags by number of films

--- app/Http/Livewire/Tags.php
+++ app/Http/Livewire/Tags.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Tag;
 use Livewire\Component;
+use Illuminate\Support\Collection;
 
 class Tags extends Component
 {
+    /** @var Collection */
+    public $tags;
+
+    public function mount()
+    {
+        $this->tags = Tag::query()
+            ->withCount('films')
+            ->orderByDesc('films_count')
+            ->orderBy('name')
+            ->limit(50)
+            ->get()
+        ;
+    }
+
     public function render()
     {
         return view('livewire.tags');
--- resources/views/livewire/tags.blade.php
+++ resources/views/livewire/tags.blade.php
@@ -1,1 +1,11 @@
-<div>This is the tags view</div>
+<div class="max-w-6xl mx-auto">
+    <div class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6">
+        <h1 class="font-bold text-2xl">Top 50 tags by number of films</h1>
+        
+        <div>
+            @foreach($tags as $tag)
+                <a href="{{ route('tag', ['tag' => $tag]) }}" class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5">{{ $tag->name }} x{{ $tag->films_count }}</a>
+            @endforeach
+        </div>
+    </div>
+</div>
