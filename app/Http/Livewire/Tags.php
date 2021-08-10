5fbbd8a9a0b780fedba8c8afafc81e07da2e3289
PhilipVigus <philipvigus@yahoo.co.uk>
2021-08-10 07:32:23 +0100

    Add popular and filtered tags

--- app/Http/Livewire/Tags.php
+++ app/Http/Livewire/Tags.php
@@ -5,21 +5,45 @@ namespace App\Http\Livewire;
 use App\Models\Tag;
 use Livewire\Component;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 
 class Tags extends Component
 {
     /** @var Collection */
-    public $tags;
+    public $mostCommonTags;
+
+    /** @var Collection */
+    public $ignoredFilmTags;
+
+    /** @var Collection */
+    public $ignoredTrailerTags;
+
+    /** @var Collection */
+    public $allTags;
 
     public function mount()
     {
-        $this->tags = Tag::query()
+        $this->mostCommonTags = Tag::query()
             ->withCount('films')
             ->orderByDesc('films_count')
             ->orderBy('name')
             ->limit(50)
             ->get()
         ;
+
+        $this->ignoredFilmTags = Auth::user()
+            ->ignoredFilmTags()
+            ->withCount('films')
+            ->get()
+        ;
+
+        $this->ignoredTrailerTags = Auth::user()
+            ->ignoredTrailerTags()
+            ->withCount('films')
+            ->get()
+        ;
+
+        $this->allTags = Tag::all();
     }
 
     public function render()
--- resources/views/livewire/tags.blade.php
+++ resources/views/livewire/tags.blade.php
@@ -1,9 +1,52 @@
-<div class="max-w-6xl mx-auto">
+<div
+    class="max-w-6xl mx-auto" 
+    x-data="{ 
+        filterTerm: '',
+        tags: {{ $allTags }},
+        filteredTags: {{ $allTags }},
+        updateFilter() {
+            this.filteredTags = this.tags.filter((tag) => {
+                return tag.name.toLowerCase().includes(this.filterTerm.toLowerCase());
+            });
+        },
+        getTagUrl(tag) {
+            return 'tags/' + tag.slug;
+        }
+    }"
+>
+    <div class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6">   
+        <input class="w-full mb-4" type="search" x-model="filterTerm" x-on:input="updateFilter()"></input>
+
+        <template x-for="tag in filteredTags" :key="tag.slug">
+            <a x-bind:href="getTagUrl(tag)" class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5" x-text="tag.name"></a>
+        </template>
+    </div>
+
     <div class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6">
-        <h1 class="font-bold text-2xl">Top 50 tags by number of films</h1>
+        <h2 class="font-bold text-2xl">Top 50 tags by number of films</h2>
         
         <div>
-            @foreach($tags as $tag)
+            @foreach($mostCommonTags as $tag)
+                <a href="{{ route('tag', ['tag' => $tag]) }}" class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5">{{ $tag->name }} x{{ $tag->films_count }}</a>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6">
+        <h2 class="font-bold text-2xl">Ignored tags</h2>
+        
+        <div class="mt-2">
+            <h3 class="font-bold text-xl">Film</h3>
+
+            @foreach($ignoredFilmTags as $tag)
+                <a href="{{ route('tag', ['tag' => $tag]) }}" class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5">{{ $tag->name }} x{{ $tag->films_count }}</a>
+            @endforeach
+        </div>
+
+        <div class="mt-2">
+            <h3 class="font-bold text-xl">Trailer</h3>
+
+            @foreach($ignoredTrailerTags as $tag)
                 <a href="{{ route('tag', ['tag' => $tag]) }}" class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5">{{ $tag->name }} x{{ $tag->films_count }}</a>
             @endforeach
         </div>
