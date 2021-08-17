a09b7381f908f9dbd91c6e3a06f95b27f0587635
PhilipVigus <philipvigus@yahoo.co.uk>
2021-08-17 07:18:19 +0100

    Move film tags to partial

---
+++ resources/views/livewire/partials/ignoredFilmTags.blade.php
@@ -0,0 +1,47 @@
+<div 
+    class="relative mt-4"
+    x-data="{ 
+        filterTerm: '',
+        tags: {{ $allTags }},
+        filteredTags: {{ $allTags }},
+        ignoredFilmTagIds: {{ $ignoredFilmTagIds }},
+        updateFilter() {
+            this.filteredTags = this.tags.filter((tag) => {
+                return tag.name.toLowerCase().includes(this.filterTerm.toLowerCase());
+            });
+        },
+        toggleIgnoredFilmTag(tag) {
+            if (this.isIgnored(tag)) {
+                const index = this.ignoredFilmTagIds.indexOf(tag.id);
+                this.ignoredFilmTagIds.splice(index, 1);
+            } else {
+                this.ignoredFilmTagIds.push(tag.id);
+            }
+
+            this.$wire.toggleIgnoredFilmTag(tag);
+        },
+        isIgnored(tag) {
+            return this.ignoredFilmTagIds.includes(tag.id);
+        }
+    }"
+>
+    <h3 class="font-bold text-lg">Film</h3>
+
+    <input class="w-full mb-2" type="search" x-model="filterTerm" x-on:input="updateFilter()"></input>
+
+    <div class="bg-white border border-black rounded absolute w-full p-4" x-show="filterTerm !=''">
+        <template x-for="tag in filteredTags" :key="tag.slug">
+            <button 
+                class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5"
+                x-bind:class="{ 'bg-green-300': isIgnored(tag) }"
+                x-text="tag.name" 
+                x-on:click="toggleIgnoredFilmTag(tag)"
+            >
+            </button>
+        </template>
+    </div>
+
+    <template x-for="tag in tags.filter((t) => ignoredFilmTagIds.includes(t.id))" :key="tag.slug">
+        <button class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5" x-text="tag.name" x-on:click="toggleIgnoredFilmTag(tag)"></button>
+    </template>
+</div>
--- resources/views/livewire/tags.blade.php
+++ resources/views/livewire/tags.blade.php
@@ -1,76 +1,10 @@
 <div
     class="max-w-6xl mx-auto" 
-    x-data="{ 
-        filterTerm: '',
-        tags: {{ $allTags }},
-        filteredTags: {{ $allTags }},
-        ignoredFilmTagIds: {{ $ignoredFilmTagIds }},
-        updateFilter() {
-            this.filteredTags = this.tags.filter((tag) => {
-                return tag.name.toLowerCase().includes(this.filterTerm.toLowerCase());
-            });
-        },
-        toggleIgnoredFilmTag(tag) {
-            if (this.isIgnored(tag)) {
-                const index = this.ignoredFilmTagIds.indexOf(tag.id);
-                this.ignoredFilmTagIds.splice(index, 1);
-            } else {
-                this.ignoredFilmTagIds.push(tag.id);
-            }
-
-            this.$wire.toggleIgnoredFilmTag(tag);
-        },
-        isIgnored(tag) {
-            return this.ignoredFilmTagIds.includes(tag.id);
-        }
-    }"
 >
     <div class="mt-8 bg-gray-200 h-auto shadow-md rounded-md p-6">
         <h2 class="font-bold text-2xl">Manage tags</h2>
 
-        <div class="relative mt-4">
-            <h3 class="font-bold text-lg">Film</h3>
-   
-            <input class="w-full mb-2" type="search" x-model="filterTerm" x-on:input="updateFilter()"></input>
-
-            <div class="bg-white border border-black rounded absolute w-full p-4" x-show="filterTerm !=''">
-                <template x-for="tag in filteredTags" :key="tag.slug">
-                    <button 
-                        class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5"
-                        x-bind:class="{ 'bg-green-300': isIgnored(tag) }"
-                        x-text="tag.name" 
-                        x-on:click="toggleIgnoredFilmTag(tag)"
-                    >
-                    </button>
-                </template>
-            </div>
-
-            <template x-for="tag in tags.filter((t) => ignoredFilmTagIds.includes(t.id))" :key="tag.slug">
-                <button class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5" x-text="tag.name" x-on:click="toggleIgnoredFilmTag(tag)"></button>
-            </template>
-        </div>
-
-        <div class="relative mt-4">
-            <h3 class="font-bold text-lg">Trailer</h3>
-   
-            <input class="w-full mb-2" type="search" x-model="filterTerm" x-on:input="updateFilter()"></input>
-
-            <div class="bg-white border border-black rounded absolute w-full p-4" x-show="filterTerm !=''">
-                <template x-for="tag in filteredTags" :key="tag.slug">
-                    <button 
-                        class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5"
-                        x-bind:class="{ 'bg-green-300': isIgnored(tag) }"
-                        x-text="tag.name" 
-                        x-on:click="toggleIgnoredFilmTag(tag)"
-                    >
-                    </button>
-                </template>
-            </div>
-
-            <template x-for="tag in tags.filter((t) => ignoredFilmTagIds.includes(t.id))" :key="tag.slug">
-                <button class="inline-flex bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5" x-text="tag.name" x-on:click="toggleIgnoredFilmTag(tag)"></button>
-            </template>
-        </div>
+        @include ('livewire.partials.ignoredFilmTags')
     </div>
 
     <div class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6">
