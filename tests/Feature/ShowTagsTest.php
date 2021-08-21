43bc69f5438d01e101a2ed4803e7dcf9ec09575a
PhilipVigus <philipvigus@yahoo.co.uk>
2021-08-21 12:06:22 +0100

    Add tests

--- tests/Feature/ShowTagsTest.php
+++ tests/Feature/ShowTagsTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Models\Tag;
 use Tests\TestCase;
 use App\Models\User;
+use Livewire\Livewire;
+use App\Http\Livewire\Tags;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ShowTagsTest extends TestCase
@@ -27,8 +29,72 @@ class ShowTagsTest extends TestCase
     {
         $tag = Tag::factory()->create();
 
-        $response = $this->get(route('tag', ['tag' => $tag]));
+        $response = $this->get(route('tags'));
 
         $response->assertRedirect('login');
+    }
+
+    /** @test */
+    public function you_can_ignore_a_film_tag()
+    {
+        $user = User::factory()->create();
+
+        $tag = Tag::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(Tags::class)
+            ->call('toggleIgnoredFilmTag', $tag)
+        ;
+
+        $this->assertCount(1, $user->ignoredFilmTags);
+        $this->assertEquals($tag->id, $user->ignoredFilmTags->first()->id);
+    }
+
+    /** @test */
+    public function you_can_unignore_a_film_tag()
+    {
+        $user = User::factory()->create();
+
+        $tag = Tag::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(Tags::class)
+            ->call('toggleIgnoredFilmTag', $tag)
+            ->call('toggleIgnoredFilmTag', $tag)
+        ;
+
+        $this->assertEmpty($user->ignoredFilmTags);
+    }
+
+    /** @test */
+    public function you_can_ignore_a_trailer_tag()
+    {
+        $user = User::factory()->create();
+
+        $tag = Tag::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(Tags::class)
+            ->call('toggleIgnoredTrailerTag', $tag)
+        ;
+
+        $this->assertCount(1, $user->ignoredTrailerTags);
+        $this->assertEquals($tag->id, $user->ignoredTrailerTags->first()->id);
+    }
+
+    /** @test */
+    public function you_can_unignore_a_trailer_tag()
+    {
+        $user = User::factory()->create();
+
+        $tag = Tag::factory()->create();
+
+        Livewire::actingAs($user)
+            ->test(Tags::class)
+            ->call('toggleIgnoredTrailerTag', $tag)
+            ->call('toggleIgnoredTrailerTag', $tag)
+        ;
+
+        $this->assertEmpty($user->ignoredTrailerTags);
     }
 }
