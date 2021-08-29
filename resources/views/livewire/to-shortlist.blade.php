79e007284a746b53e179e25137499d784311169f
PhilipVigus <philipvigus@yahoo.co.uk>
2021-08-29 12:29:57 +0100

    Add basic gui element

--- resources/views/livewire/to-shortlist.blade.php
+++ resources/views/livewire/to-shortlist.blade.php
@@ -1,4 +1,13 @@
 <div class="max-w-6xl mx-auto">
+    <div class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6 flex items-center relative">
+
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 absolute right-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+
+        <input class="w-full" type="search"></input>
+    </div>
+
     @forelse ($films as $film)
         @include ('livewire.partials._toShortlistFilm', ['film' => $film, 'loop' => $loop])
     @empty
