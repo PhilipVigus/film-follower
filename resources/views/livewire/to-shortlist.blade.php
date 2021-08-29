a2b429d16615b32a831c80076aec6c9cede746dd
PhilipVigus <philipvigus@yahoo.co.uk>
2021-08-29 13:59:28 +0100

    Add alpine text filter

--- resources/views/livewire/to-shortlist.blade.php
+++ resources/views/livewire/to-shortlist.blade.php
@@ -1,14 +1,26 @@
-<div class="max-w-6xl mx-auto" x-data="{ films: {{ $films }} }">
+<div 
+    class="max-w-6xl mx-auto" 
+    x-data="{ 
+        films: {{ $films }},
+        filteredFilms: {{ $films }},
+        searchTerm: '',
+        updateFilter() {
+            this.filteredFilms = this.films.filter((film) => {
+                return film['title'].toLowerCase().includes(this.searchTerm.toLowerCase());
+            });
+        }
+    }"
+>
     <div class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6 flex items-center relative">
 
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 absolute right-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
 
-        <input class="w-full" type="search" placeholder="Search..."></input>
+        <input class="w-full" type="search" placeholder="Search..." x-model="searchTerm" x-on:input="updateFilter()"></input>
     </div>
 
-    <template x-for="(film, index) in films" :key="index">
+    <template x-for="(film, index) in filteredFilms" :key="index">
         <article class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6">
             <h2 class="font-bold text-2xl" x-text="film['title']"></h2>
 
