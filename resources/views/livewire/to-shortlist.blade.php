3724ff487470d29688af6b0bdbe1dc03d669a65d
PhilipVigus <philipvigus@yahoo.co.uk>
2021-08-29 12:32:13 +0100

    Add placeholder

--- resources/views/livewire/to-shortlist.blade.php
+++ resources/views/livewire/to-shortlist.blade.php
@@ -5,7 +5,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
 
-        <input class="w-full" type="search"></input>
+        <input class="w-full" type="search" placeholder="Search..."></input>
     </div>
 
     @forelse ($films as $film)
