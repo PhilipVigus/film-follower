fdf31d579b702316824dc3778f6ecc2bec03ac07
PhilipVigus <philipvigus@yahoo.co.uk>
2021-09-25 13:35:59 +0100

    Import films

--- app/Console/Commands/ImportLegacyData.php
+++ app/Console/Commands/ImportLegacyData.php
@@ -2,16 +2,19 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
+use App\Models\Film;
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
 class ImportLegacyData extends Command
 {
     /** @var string */
-    protected $signature = 'film-follower:import-legacy-data {file}';
+    protected $signature = 'film-follower:import-legacy-data';
 
     /** @var string */
-    protected $description = 'This command imports data from the previous version of film-follower, which is in json format.';
+    protected $description = 'This command imports data from the mongodb database of the previous version of film-follower.';
 
     public function __construct()
     {
@@ -22,8 +25,27 @@ class ImportLegacyData extends Command
     public function handle()
     {
         $trailers = DB::connection('mongodb')->table('trailers')->get();
-        dd($trailers->first());
-        $this->info(json_encode($trailers->first()));
+
+        foreach ($trailers as $trailer) {
+            $guid = Str::beforeLast($trailer['guid'], '/');
+
+            if (! Str::contains($guid, 'http://')) {
+                continue;
+            }
+
+            $title = Str::beforeLast($trailer['title'], ':');
+            $date = Carbon::createFromTimestampMs($trailer['articleDate']);
+            $film = Film::firstOrCreate(
+                [
+                    'guid' => $guid,
+                ],
+                [
+                    'title' => $title,
+                    'created_at' => $date,
+                    'updated_at' => $date,
+                ]
+            );
+        }
 
         return 0;
     }
