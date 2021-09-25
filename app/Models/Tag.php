f4a11f545521705fd38e9e3c6a643413b7416263
PhilipVigus <philipvigus@yahoo.co.uk>
2021-09-25 13:35:36 +0100

    Fix duplicate slugs in tags

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,14 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
+    /** @var array */
     protected $guarded = [];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+            ],
+        ];
+    }
 
     public function films(): BelongsToMany
     {
