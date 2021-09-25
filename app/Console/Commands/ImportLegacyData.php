a2a4b8cc2dca9da6df2087feac9a13db8af3467c
PhilipVigus <philipvigus@yahoo.co.uk>
2021-09-25 14:56:57 +0100

    Import tags

--- app/Console/Commands/ImportLegacyData.php
+++ app/Console/Commands/ImportLegacyData.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Carbon\Carbon;
+use App\Models\Tag;
 use App\Models\Film;
 use App\Models\User;
 use Illuminate\Support\Str;
@@ -17,6 +18,13 @@ class ImportLegacyData extends Command
     /** @var string */
     protected $description = 'This command imports data from the mongodb database of the previous version of film-follower.';
 
+    /** @array */
+    protected $ratingMapping = [
+        1 => 1,
+        2 => 3,
+        3 => 5,
+    ];
+
     public function __construct()
     {
         parent::__construct();
@@ -28,13 +36,13 @@ class ImportLegacyData extends Command
         $trailers = DB::connection('mongodb')->table('trailers')->get();
 
         foreach ($trailers as $trailer) {
-            $filmGuid = Str::beforeLast($trailer['guid'], '/');
+            $filmGuid = $this->decodeRssString(Str::beforeLast($trailer['guid'], '/'));
 
             if (! Str::contains($filmGuid, 'http://')) {
                 continue;
             }
 
-            $filmTitle = Str::beforeLast($trailer['title'], ':');
+            $filmTitle = $this->decodeRssString(Str::beforeLast($trailer['title'], ':'));
             $date = Carbon::createFromTimestampMs($trailer['articleDate']);
 
             $film = Film::firstOrCreate(
@@ -56,6 +64,16 @@ class ImportLegacyData extends Command
                 }
             }
 
+            $me = $users->first();
+
+            if (isset($trailer['rating'])) {
+                $rating = $this->ratingMapping[$trailer['rating']];
+                $comment = $trailer['notes'] ?? '';
+
+                $me->priorities()->create(['film_id' => $film->id, 'rating' => $rating, 'comment' => $comment]);
+                $me->films()->updateExistingPivot($film->id, ['status' => Film::SHORTLISTED]);
+            }
+
             $trailerType = trim(Str::afterLast($trailer['title'], ':'));
 
             $film->trailers()->create([
@@ -68,8 +86,75 @@ class ImportLegacyData extends Command
                 'created_at' => $date,
                 'updated_at' => $date,
             ]);
+
+            $tags = explode(', ', $trailer['tags']);
+
+            foreach ($tags as $tagData) {
+                $tag = Tag::firstOrCreate(
+                    [
+                        'name' => $this->decodeRssString($tagData),
+                    ],
+                );
+
+                if (! $film->tags->contains($tag)) {
+                    $film->tags()->attach($tag);
+                }
+            }
         }
 
         return 0;
+    }
+
+    private function decodeRssString($string)
+    {
+        return $this->fixWrongUTF8Encoding(mb_convert_encoding($string, 'UTF-8'));
+    }
+
+    private function fixWrongUTF8Encoding($inputString)
+    {
+        // code source:  https://github.com/devgeniem/wp-sanitize-accented-uploads/blob/master/plugin.php#L152
+        // table source: http://www.i18nqa.com/debug/utf8-debug.html
+
+        $fix_list = [
+            // 3 char errors first
+            'â€š' => '‚', 'â€ž' => '„', 'â€¦' => '…', 'â€¡' => '‡',
+            'â€°' => '‰', 'â€¹' => '‹', 'â€˜' => '‘', 'â€™' => '’',
+            'â€œ' => '“', 'â€¢' => '•', 'â€“' => '–', 'â€”' => '—',
+            'â„¢' => '™', 'â€º' => '›', 'â‚¬' => '€',
+            // 2 char errors
+            'Ã‚' => 'Â', 'Æ’' => 'ƒ', 'Ãƒ' => 'Ã', 'Ã„' => 'Ä',
+            'Ã…' => 'Å', 'â€' => '†', 'Ã†' => 'Æ', 'Ã‡' => 'Ç',
+            'Ë†' => 'ˆ', 'Ãˆ' => 'È', 'Ã‰' => 'É', 'ÃŠ' => 'Ê',
+            'Ã‹' => 'Ë', 'Å’' => 'Œ', 'ÃŒ' => 'Ì', 'Å½' => 'Ž',
+            'ÃŽ' => 'Î', 'Ã‘' => 'Ñ', 'Ã’' => 'Ò', 'Ã“' => 'Ó',
+            'â€' => '”', 'Ã”' => 'Ô', 'Ã•' => 'Õ', 'Ã–' => 'Ö',
+            'Ã—' => '×', 'Ëœ' => '˜', 'Ã˜' => 'Ø', 'Ã™' => 'Ù',
+            'Å¡' => 'š', 'Ãš' => 'Ú', 'Ã›' => 'Û', 'Å“' => 'œ',
+            'Ãœ' => 'Ü', 'Å¾' => 'ž', 'Ãž' => 'Þ', 'Å¸' => 'Ÿ',
+            'ÃŸ' => 'ß', 'Â¡' => '¡', 'Ã¡' => 'á', 'Â¢' => '¢',
+            'Ã¢' => 'â', 'Â£' => '£', 'Ã£' => 'ã', 'Â¤' => '¤',
+            'Ã¤' => 'ä', 'Â¥' => '¥', 'Ã¥' => 'å', 'Â¦' => '¦',
+            'Ã¦' => 'æ', 'Â§' => '§', 'Ã§' => 'ç', 'Â¨' => '¨',
+            'Ã¨' => 'è', 'Â©' => '©', 'Ã©' => 'é', 'Âª' => 'ª',
+            'Ãª' => 'ê', 'Â«' => '«', 'Ã«' => 'ë', 'Â¬' => '¬',
+            'Ã¬' => 'ì', 'Â®' => '®', 'Ã®' => 'î', 'Â¯' => '¯',
+            'Ã¯' => 'ï', 'Â°' => '°', 'Ã°' => 'ð', 'Â±' => '±',
+            'Ã±' => 'ñ', 'Â²' => '²', 'Ã²' => 'ò', 'Â³' => '³',
+            'Ã³' => 'ó', 'Â´' => '´', 'Ã´' => 'ô', 'Âµ' => 'µ',
+            'Ãµ' => 'õ', 'Â¶' => '¶', 'Ã¶' => 'ö', 'Â·' => '·',
+            'Ã·' => '÷', 'Â¸' => '¸', 'Ã¸' => 'ø', 'Â¹' => '¹',
+            'Ã¹' => 'ù', 'Âº' => 'º', 'Ãº' => 'ú', 'Â»' => '»',
+            'Ã»' => 'û', 'Â¼' => '¼', 'Ã¼' => 'ü', 'Â½' => '½',
+            'Ã½' => 'ý', 'Â¾' => '¾', 'Ã¾' => 'þ', 'Â¿' => '¿',
+            'Ã¿' => 'ÿ', 'Ã€' => 'À',
+            // 1 char errors last
+            'Ã' => 'Á', 'Å' => 'Š', 'Ã' => 'Í', 'Ã' => 'Ï',
+            'Ã' => 'Ð', 'Ã' => 'Ý', 'Ã' => 'à', 'Ã­' => 'í',
+        ];
+
+        $error_chars = array_keys($fix_list);
+        $real_chars = array_values($fix_list);
+
+        return str_replace($error_chars, $real_chars, $inputString);
     }
 }
