e31607e71c38621b4300cdb68ebb45283fea6557
PhilipVigus <philipvigus@yahoo.co.uk>
2021-09-25 13:53:02 +0100

    Import trailers

--- app/Console/Commands/ImportLegacyData.php
+++ app/Console/Commands/ImportLegacyData.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Carbon\Carbon;
 use App\Models\Film;
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -27,24 +28,46 @@ class ImportLegacyData extends Command
         $trailers = DB::connection('mongodb')->table('trailers')->get();
 
         foreach ($trailers as $trailer) {
-            $guid = Str::beforeLast($trailer['guid'], '/');
+            $filmGuid = Str::beforeLast($trailer['guid'], '/');
 
-            if (! Str::contains($guid, 'http://')) {
+            if (! Str::contains($filmGuid, 'http://')) {
                 continue;
             }
 
-            $title = Str::beforeLast($trailer['title'], ':');
+            $filmTitle = Str::beforeLast($trailer['title'], ':');
             $date = Carbon::createFromTimestampMs($trailer['articleDate']);
+
             $film = Film::firstOrCreate(
                 [
-                    'guid' => $guid,
+                    'guid' => $filmGuid,
                 ],
                 [
-                    'title' => $title,
+                    'title' => $filmTitle,
                     'created_at' => $date,
                     'updated_at' => $date,
                 ]
             );
+
+            $users = User::all();
+
+            foreach ($users as $user) {
+                if (! $user->films()->where('film_id', $film->id)->exists()) {
+                    $user->films()->attach($film);
+                }
+            }
+
+            $trailerType = trim(Str::afterLast($trailer['title'], ':'));
+
+            $film->trailers()->create([
+                'guid' => $trailer['guid'],
+                'title' => $trailer['title'],
+                'type' => $trailerType,
+                'image' => $trailer['imageURL'],
+                'link' => $trailer['trailerLink'],
+                'uploaded_at' => $date,
+                'created_at' => $date,
+                'updated_at' => $date,
+            ]);
         }
 
         return 0;
