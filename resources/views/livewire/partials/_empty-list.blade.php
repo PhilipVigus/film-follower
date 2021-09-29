adcbb86441bf7214d12c744dba742ac162009654
PhilipVigus <philipvigus@yahoo.co.uk>
2021-09-29 07:41:28 +0100

    Fix empty film lists

--- resources/views/livewire/partials/_empty-list.blade.php
+++ resources/views/livewire/partials/_empty-list.blade.php
@@ -1,3 +1,3 @@
-<div class="mt-4 border" x-show="!films.length">
-    <div class="font-bold text-lg">None</div>
+<div class="mt-8 bg-gray-200 h-auto shadow-md overflow-hidden rounded-md p-6" x-show="!films.length">
+    <div class="font-bold text-lg">You have no films in this list</div>
 </div>
