32be205d1725f2a4cb6876b72eb61cedaac0b718
PhilipVigus <philipvigus@yahoo.co.uk>
2021-10-01 07:09:06 +0100

    Add top margin

--- resources/views/livewire/partials/_footer.blade.php
+++ resources/views/livewire/partials/_footer.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-gray-200 text-sm border-b border-gray-100 flex space-x-2 justify-center items-center p-4" x-data="{ year: new Date().getFullYear() }">
+<div class="bg-gray-200 mt-8 text-sm border-b border-gray-100 flex space-x-2 justify-center items-center p-4" x-data="{ year: new Date().getFullYear() }">
   <span x-text="'Phil Vigus &copy; ' + year"></span>
   <span>&middot;</span>
   <a href="https://www.philvigus.com" target="_blank">philvigus.com</a>
