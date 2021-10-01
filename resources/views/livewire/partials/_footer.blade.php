0deef94c4d7fc277d5baaa422add9feba211005d
PhilipVigus <philipvigus@yahoo.co.uk>
2021-10-01 07:08:14 +0100

    Tweak wording

--- resources/views/livewire/partials/_footer.blade.php
+++ resources/views/livewire/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-gray-200 text-sm border-b border-gray-100 flex space-x-2 justify-center items-center p-4" x-data="{ year: new Date().getFullYear() }">
   <span x-text="'Phil Vigus &copy; ' + year"></span>
   <span>&middot;</span>
-  <a href="https://www.philvigus.com" target="_blank">Portfolio</a>
+  <a href="https://www.philvigus.com" target="_blank">philvigus.com</a>
 </div>
