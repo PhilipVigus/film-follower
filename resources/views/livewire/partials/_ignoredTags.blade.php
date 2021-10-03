4e1853aec95388cadc07a405db219cd0b8084d93
PhilipVigus <philipvigus@yahoo.co.uk>
2021-10-03 11:56:30 +0100

    Limit number of tags showing in ignored tag search

--- resources/views/livewire/partials/_ignoredTags.blade.php
+++ resources/views/livewire/partials/_ignoredTags.blade.php
@@ -5,11 +5,26 @@
         tags: {{ $allTags }},
         filteredTags: {{ $allTags }},
         ignoredFilmTagIds: {{ $ignoredFilmTagIds }},
+        numberOfExcessTags: 0,
         updateFilter() {
+            if (this.filterTerm === '') {
+                this.filteredTags = this.tags;
+                this.numberOfExcessTags = 0;
+                return;
+            }
+
             this.filteredTags = this.tags.filter((tag) => {
                 return tag.name.toLowerCase().includes(this.filterTerm.toLowerCase())
                     && !this.ignoredFilmTagIds.includes(tag.id);
             });
+
+            if (this.filteredTags.length > 50) {
+                this.numberOfExcessTags = this.filteredTags.length - 50;
+                this.filteredTags = this.filteredTags.splice(this.numberOfExcessTags);
+                return;
+            }
+                
+            this.numberOfExcessTags = 0;
         },
         toggleIgnoredFilmTag(tag) {
             if (this.isIgnored(tag)) {
@@ -33,7 +48,7 @@
         <button class="inline-flex bg-green-300 rounded-full px-2 py-1 mr-2 mt-1.5 hover:bg-gray-400" x-text="tag.name" x-on:click="toggleIgnoredFilmTag(tag); updateFilter()"></button>
     </template>
     
-    <input class="w-full mt-4" type="search" placeholder="Search tags to add to the list" x-model="filterTerm" x-on:input="updateFilter()"></input>
+    <input class="w-full mt-4" type="search" placeholder="Search tags to add to the list" x-model="filterTerm" x-on:input="updateFilter()" x-on:click.away="filterTerm = ''"></input>
 
     <div class="bg-white border border-black rounded absolute w-full p-4 z-10" x-show="filterTerm !=''" x-cloak>
         <template x-for="tag in filteredTags" :key="tag.slug">
@@ -45,6 +60,8 @@
             >
             </button>
         </template>
+
+        <span class="bg-gray-400 rounded-full px-2 py-1 mr-2 mt-1.5" x-show="numberOfExcessTags > 0" x-text="`+ ${numberOfExcessTags} tag(s)`"></span>
     </div>
 
 </div>
